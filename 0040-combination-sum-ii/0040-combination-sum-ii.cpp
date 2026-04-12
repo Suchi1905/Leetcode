b3aa45f7a84ff3d4eb081b69cd11535984fa2dec
Suchi Shukla <suchi.shukla1905@gmail.com>
2026-04-12 21:39:23 +0530

    Time: 0 ms (100%), Space: 14 MB (81.81%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,39 +1,26 @@
 class Solution {
-public:
-    
+public: 
     void solve(vector<int>& candidates, int index, int target,
-               vector<int>& ds, vector<vector<int>>& results){
-        
+               vector<int>& ds, vector<vector<int>>& results){  
         if(target == 0){
             results.push_back(ds);
             return;
         }
-        
         for(int i = index; i < candidates.size(); i++){
-            
-            // skip duplicates
             if(i > index && candidates[i] == candidates[i-1])
-                continue;
-            
+                continue;       
             if(candidates[i] > target)
-                break;
-            
+                break;         
             ds.push_back(candidates[i]);
             solve(candidates, i+1, target - candidates[i], ds, results);
             ds.pop_back();
         }
     }
-    
-    
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {   
         vector<vector<int>> results;
-        vector<int> ds;
-        
-        sort(candidates.begin(), candidates.end());
-        
-        solve(candidates, 0, target, ds, results);
-        
+        vector<int> ds;  
+        sort(candidates.begin(), candidates.end());   
+        solve(candidates, 0, target, ds, results);  
         return results;
     }
 };
