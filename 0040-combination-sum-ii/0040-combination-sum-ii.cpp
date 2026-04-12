14098f98d6185d0181447770c85f99c10885b7f6
Suchi Shukla <suchi.shukla1905@gmail.com>
2026-04-12 21:38:30 +0530

    Time: 3 ms (37.16%), Space: 14 MB (59.73%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        int index;
-        vector<int>ds;
-        vector<vector<int>>results;
-        sort(candidates.begin(), candidates.end());
-        results=solve(candidates,0,target,ds,results);
-        return results;
-    }
-    vector<vector<int>>solve(vector<int> &candidates,int index,int target,vector<int>ds,vector<vector<int>>results){
-        if(target==0){
+    
+    void solve(vector<int>& candidates, int index, int target,
+               vector<int>& ds, vector<vector<int>>& results){
+        
+        if(target == 0){
             results.push_back(ds);
-            return results;
+            return;
         }
-        if(index >= candidates.size())
-        return results;
-        if(candidates[index]<=target){
-            if(index==0 || candidates[index-1] != candidates[index] || (ds.size() && ds.back()==candidates[index-1])){
-            ds.push_back(candidates[index]);
-            results=solve(candidates,index+1,target-candidates[index],ds,results);
+        
+        for(int i = index; i < candidates.size(); i++){
+            
+            // skip duplicates
+            if(i > index && candidates[i] == candidates[i-1])
+                continue;
+            
+            if(candidates[i] > target)
+                break;
+            
+            ds.push_back(candidates[i]);
+            solve(candidates, i+1, target - candidates[i], ds, results);
             ds.pop_back();
-            }
         }
-        int next = index;
-        while(next+1 < candidates.size() && candidates[next] == candidates[next+1])
-            next++;
-        results=solve(candidates,next+1,target,ds,results);
+    }
+    
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        vector<vector<int>> results;
+        vector<int> ds;
+        
+        sort(candidates.begin(), candidates.end());
+        
+        solve(candidates, 0, target, ds, results);
+        
         return results;
     }
 };
